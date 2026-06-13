aae5915ae402bfeed9d313925b2b25d87c50530c
younessedd <younesseddanguir@gmail.com>
2026-06-13 15:09:41 +0100

    Remove terser config, use Vite's default esbuild minifier

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
     rollupOptions: {
       output: {
         manualChunks: {
